0b453f98bae58d452ef31d1f6f833c2e9e605a69
Tomáš Kotula <tomasrkotula@gmail.com>
2026-08-05 09:29:50 +0200

    Remove greeting. Center and limit width

--- app/shared/src/commonMain/kotlin/cz/kotu/game/gotfive/App.kt
+++ app/shared/src/commonMain/kotlin/cz/kotu/game/gotfive/App.kt
@@ -1,33 +1,25 @@
 package cz.kotu.game.gotfive
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import org.jetbrains.compose.resources.painterResource
-
-import gotfive.app.shared.generated.resources.Res
-import gotfive.app.shared.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         val gameViewModel: GameViewModel = viewModel(factory = GameViewModelFactory)
-        var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -36,20 +28,11 @@ fun App() {
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            Table(gameViewModel, Modifier.fillMaxWidth())
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            Table(
+                gameViewModel, Modifier
+                    .width(960.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
