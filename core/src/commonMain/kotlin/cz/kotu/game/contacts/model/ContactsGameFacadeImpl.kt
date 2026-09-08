452d3762e9a7327569db1d5c68197cb406d8a5ca
Tomáš Kotula <tomasrkotula@gmail.com>
2026-09-08 10:08:06 +0200

    Extract ContactsGameReducers.kt

--- core/src/commonMain/kotlin/cz/kotu/game/contacts/model/ContactsGameFacadeImpl.kt
+++ core/src/commonMain/kotlin/cz/kotu/game/contacts/model/ContactsGameFacadeImpl.kt
@@ -1,7 +1,5 @@
 package cz.kotu.game.contacts.model
 
-import cz.kotu.game.contacts.model.ContactsGameState.GamePhase.GameOver
-import cz.kotu.game.contacts.model.ContactsGameState.GamePhase.ResolveMultiConnect
 import cz.kotu.game.contacts.model.ContactsGameState.GamePhase.StandardTurn
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -60,77 +58,6 @@ class ContactsGameFacadeImpl(
             Result.failure(error)
         }
     }
-
-    private fun ContactsGameState.applyAction(
-        player: ContactsBoardState.Player,
-        actionType: ContactsBoardState.ActionType,
-        playerContacts: Set<ContactsBoardState.ContactId>,
-        otherContacts: Set<ContactsBoardState.ContactId>,
-        addRichGameLog: (LogBuilder.() -> Unit) -> Unit,
-    ): ContactsGameState {
-        val result = board.applyActionIds(player, actionType, playerContacts, otherContacts)
-        val nextState = copy(
-            board = result.state,
-            gamePhase = nextGamePhase(actionType, result.state),
-        )
-        addRichGameLog(result.logBuilder)
-        return nextState
-    }
-
-    private fun ContactsGameState.nextGamePhase(
-        actionType: ContactsBoardState.ActionType,
-        newBoard: ContactsBoardState,
-    ): ContactsGameState.GamePhase {
-        return when (gamePhase) {
-            is StandardTurn -> {
-                when (actionType) {
-                    ContactsBoardState.ActionType.StandardConnect,
-                    ContactsBoardState.ActionType.AddHint,
-                    ContactsBoardState.ActionType.MyDoubleConnect,
-                    ContactsBoardState.ActionType.SoloConnectRest,
-                    ContactsBoardState.ActionType.FinishReds,
-                        -> {
-                        StandardTurn(
-                            nextPlayer(gamePhase.activePlayer),
-                        )
-                    }
-
-                    ContactsBoardState.ActionType.DoubleConnect,
-                    ContactsBoardState.ActionType.TripleConnect,
-                        -> {
-                        val resolveMultiConnect = newBoard.resolveMultiConnect!!
-                        ResolveMultiConnect(
-                            restorePlayer = gamePhase.activePlayer,
-                            resolveMultiConnect = resolveMultiConnect,
-                        )
-                    }
-
-                    ContactsBoardState.ActionType.ResolveMultiConnect -> throw InvalidActionException("Not in ResolveMultiConnect phase")
-                }
-            }
-
-            is ResolveMultiConnect -> {
-                when (actionType) {
-                    // only legal from ResolveMultiConnect phase
-                    ContactsBoardState.ActionType.ResolveMultiConnect -> {
-                        StandardTurn(
-                            nextPlayer(gamePhase.restorePlayer),
-                        )
-                    }
-
-                    else -> throw InvalidActionException("Only ResolveMultiConnect")
-                }
-            }
-
-            is GameOver -> {
-                throw InvalidActionException("Game is over - no actions")
-            }
-        }
-    }
-
-    // todo only player with contacts
-    private fun ContactsGameState.nextPlayer(afterPlayer: ContactsBoardState.Player): ContactsBoardState.Player =
-        players[(players.indexOf(afterPlayer) + 1) % players.size]
 
     private fun addGameLog(text: String) {
         _logs.value += GameLogEntry(
