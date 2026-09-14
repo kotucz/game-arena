e53489fb24327d87bf7c52b45b7c5641ddb924ee
Tomáš Kotula <tomasrkotula@gmail.com>
2026-09-14 08:39:35 +0200

    @ServerScope to ensure single DB instance + others

--- server/src/main/kotlin/cz/kotu/gamearena/ServerBindings.kt
+++ server/src/main/kotlin/cz/kotu/gamearena/ServerBindings.kt
@@ -1,12 +1,18 @@
 package cz.kotu.gamearena
 
 import me.tatarka.inject.annotations.Provides
+import me.tatarka.inject.annotations.Scope
 
+@Scope
+annotation class ServerScope
+
+@ServerScope
 interface ServerBindings {
     val database: AppDatabase
     val gamesManager: GamesManager
     val serverConfig: ServerConfig
 
     @Provides
+    @ServerScope
     fun provideGamesManager(database: AppDatabase): GamesManager = GamesManager(gameDao = database.gameDao())
 }
--- server/src/main/kotlin/cz/kotu/gamearena/ServerComponent.kt
+++ server/src/main/kotlin/cz/kotu/gamearena/ServerComponent.kt
@@ -6,13 +6,14 @@ import me.tatarka.inject.annotations.Provides
 /** production server component */
 @Component
 abstract class ServerComponent(
-    @get:Provides override val serverConfig: ServerConfig,
+    @get:Provides @get:ServerScope override val serverConfig: ServerConfig,
 ) : ServerBindings {
 
     abstract override val database: AppDatabase
     abstract override val gamesManager: GamesManager
 
     @Provides
+    @ServerScope
     fun provideDatabase(serverConfig: ServerConfig): AppDatabase = createDatabase(serverConfig.databaseFile)
 
 }
