15747fdfe4d3a0ef97a46af4e72cf8ddf1d7672e
Tomáš Kotula <tomasrkotula@gmail.com>
2026-09-22 08:59:44 +0200

    Setup local and Firebase notifications for all platforms (Desktop and Web use No-op)

--- app/shared/src/iosMain/kotlin/cz/kotu/common/Notifications.ios.kt
+++ app/shared/src/iosMain/kotlin/cz/kotu/common/Notifications.ios.kt
@@ -1,8 +1,13 @@
 package cz.kotu.common
 
 import com.mmk.kmpnotifier.KMPNotifier
+import com.mmk.kmpnotifier.local.LocalNotifications
 import com.mmk.kmpnotifier.notification.configuration.NotificationPlatformConfiguration
+import com.mmk.kmpnotifier.push.firebase.FirebasePush
 
 actual fun initNotifications() {
-    KMPNotifier.initialize(NotificationPlatformConfiguration.Ios())
+    KMPNotifier.initialize(
+        NotificationPlatformConfiguration.Ios(),
+        LocalNotifications, FirebasePush,
+    )
 }
--- app/shared/src/webMain/kotlin/cz/kotu/common/Notifications.web.kt
+++ app/shared/src/webMain/kotlin/cz/kotu/common/Notifications.web.kt
@@ -1,10 +1,13 @@
 package cz.kotu.common
 
 import com.mmk.kmpnotifier.KMPNotifier
+import com.mmk.kmpnotifier.local.LocalNotifications
 import com.mmk.kmpnotifier.notification.configuration.NotificationPlatformConfiguration
+import com.mmk.kmpnotifier.push.firebase.FirebasePush
 
 actual fun initNotifications() {
     KMPNotifier.initialize(
         NotificationPlatformConfiguration.Web(),
+        LocalNotifications, FirebasePush,
     )
 }
